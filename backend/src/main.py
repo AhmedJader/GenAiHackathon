7f6dfc38c54bcd636635300b32a9bbbfe0727a0d
Yusuf-Moola <yusuf.moola@ibm.com>
2025-03-22 23:14:15 -0400

    initial flow, cors fixing, file path fixing

--- backend/src/main.py
+++ backend/src/main.py
@@ -12,14 +12,12 @@ app = FastAPI()
 # Allow requests from your frontend (Next.js running on localhost:3000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Or ["*"] for testing
+    allow_origins=["*"],# Or ["*"] for testing
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
-app = FastAPI()
 
 logging.basicConfig(filename='app.log', level=logging.INFO)
 
